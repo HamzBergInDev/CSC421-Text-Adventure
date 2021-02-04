c2af769e3a3a299bb37ffb462d4d52d57290269d
Hamz Berg <46531638+HamzBergInDev@users.noreply.github.com>
2021-02-04 12:08:50 -0500

    Create Room.java

---
+++ Room.java
@@ -0,0 +1,20 @@
+/* Hamadi Belghachi
+ * CSC 241, 2/4/2021
+ * Homework 1 (Room.java)
+ */
+ 
+/* Notables:
+ * > Room can contain ten Animals
+ * > Each room must have a name
+ * > Needs public toString() method
+ */
+ 
+ class room {
+ 
+	public static void main(String[] args){
+	 
+		System.out.println("Hello Again");
+	 
+	}
+	
+ }
